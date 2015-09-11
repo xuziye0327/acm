72f277135a38346dd8c70e7af0a17a2e4b68508a
ZmiL <xuziye0327@gmail.com>
2015-09-11 12:44:37 +0800

    Undown

---
+++ UVA/11582/11582/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  11582
+//
+//  Created by ZmiL on 15/9/11.
+//  Copyright (c) 2015年 ZmiL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
