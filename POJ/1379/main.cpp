351b89d2e66c098f4105fd61a7d1098d3efbfa70
myz <xuziye0327@gmail.com>
2015-12-02 17:00:08 +0800

    POJ 1379

---
+++ POJ/1379/main.cpp
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#define RANDOM (rand() * 1.0) / RAND_MAX
+
+using namespace std;
+
+const int maxn = 1007;
+const int INIF = 30;
+const double pi = acos(-1);
+const double eps = 1e-5;
+const double inf = 1e20;
+
+struct Point {
+    double x, y, d;
+    Point() : x(0), y(0), d(inf) { }
+}p[maxn], op[INIF];
+
+double dist(Point p1, Point p2) {
+    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int m;
+        double x, y;
+        scanf("%lf%lf%d", &x, &y, &m);
+        for (int i = 0; i < m; ++i) {
+            scanf("%lf%lf", &p[i].x, &p[i].y);
+        }
+
+        double t = sqrt(x * x + y * y);
+
+        for (int i = 0; i < INIF; ++i) {
+            op[i].x = RANDOM * x;
+            op[i].y = RANDOM * y;
+            op[i].d = inf;
+            for (int j = 0; j < m; ++j) {
+                op[i].d = min(op[i].d, dist(p[j], op[i]));
+            }
+        }
+
+        while (t > eps) {
+            for (int i = 0; i < INIF; ++i) {
+                Point tp;
+                for (int j = 0; j < INIF; ++j) {
+                    double det = RANDOM * 2.0 * pi;
+                    tp.x = op[i].x + t * cos(det);
+                    tp.y = op[i].y + t * sin(det);
+                    if (tp.x < 0 || tp.y < 0 || tp.x > x || tp.y > y) continue;
+                    for (int k = 0; k < m; ++k) {
+                        if (k == 0) tp.d = dist(tp, p[k]);
+                        else tp.d = min(tp.d, dist(tp, p[k]));
+                    }
+                    if (tp.d > op[i].d) {
+                        op[i] = tp;
+                    }
+                }
+            }
+            t *= 0.8;
+        }
+        int id = 0;
+        for (int i = 1; i < INIF; ++i) {
+            if (op[id].d < op[i].d) id = i;
+        }
+        printf("The safest point is (%.1f, %.1f).\n", op[id].x, op[id].y);
+    }
+    return 0;
+}
