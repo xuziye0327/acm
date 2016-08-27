a125d89720130c824977eba46e013b68dd86787b
ZmiL <xuziye0327@gmail.com>
2016-08-27 23:49:58 +0800

    what?

---
+++ ZOJ/2319/2319/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2319
+//
+//  Created by ZmiL on 16/1/20.
+//  Copyright © 2016年 ZmiL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
